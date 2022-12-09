1632f1470b495b1a8399fcf086b0653f0ff85a98
Phil <102290118+Phil0s@users.noreply.github.com>
2022-12-10 10:14:07 +1100

    Updated Splashscreen
    Improved GodotPixeleffect quality. Fixed frame mis sync where starting game would sometimes cause the pixeleffect to start not from the first frame.

--- Lux/Levels/Menu/Splashscreen.gd
+++ Lux/Levels/Menu/Splashscreen.gd
@@ -3,6 +3,5 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
-func _on_VideoPlayer_finished():
-	get_tree().change_scene("res://Levels/Menu/Startuptext.tscn") #When Video finished move on to next scene
+func _on_GodotLogoAnimatedSprite_animation_finished():
+	get_tree().change_scene("res://Levels/Menu/Startuptext.tscn") #When image sequence finished move on to next scene
