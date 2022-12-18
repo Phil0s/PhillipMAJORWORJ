d211bdd0ffbbe6af669f5927c30e097b5746eb38
Phil <102290118+Phil0s@users.noreply.github.com>
2022-12-18 12:41:53 +1100

    V1.2 + Tutorial Overlap
    NOTE: Next commit will be moved to tutorial branch!
    
    In this version I polished the sound setting, allowing you to change the master volume of the game.
    
    I then worked on making the play button functional, taking you to a level select page.
    
    Currently there is only one level - tutorial and I began making spritesheets for the character and level. (Not finished)

--- Lux/Levels/Menu/MainMenu.gd
+++ Lux/Levels/Menu/MainMenu.gd
@@ -1,37 +1,43 @@
 extends Node2D
 
-
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 onready var MManimationPlayer = $AnimationPlayer
 var MManimationfinished = true
 onready var startbut = $VBoxContainer/PlayButton
+onready var settingsbut = $VBoxContainer/SettingsButton
 onready var settingspage = $Control/Popup
+onready var playpage = $ControlPlay/Popup
 onready var musicslider = $Control/Popup/VBoxContainer/VBoxContainer/HSlider
+onready var Launchbutton = $ControlPlay/Popup/VBoxContainer/Launch
+
 
 func _ready():
 	_defaultreset()
 	startbut.grab_focus()
 	settingspage.visible = false
-		
-func _scrollcontainer():
-	pass
+	playpage.visible = false
 	
-func _defaultreset():
+func _defaultreset(): #Played when scene first loaded, to ensure everything starts of at the correct position and settings
 	MManimationPlayer.play("Default")
 	$AnimatedSprite.speed_scale = 1
 
-func _on_ExitButton_pressed():
+func _on_ExitButton_pressed(): #Close the app
 	print("Exit button pressed")
 	get_tree().quit()
 	
 	
-func _on_PlayButton_pressed():
+func _on_PlayButton_pressed(): #Open level select
 	print("Play Button Pressed")
+	if MManimationfinished:
+		MManimationPlayer.play("Play")
+		$AnimatedSprite.speed_scale = 1.5
+		playpage.visible = true
+		print("LevelPage visible")
+		Launchbutton.grab_focus()
 
-func _on_SettingsButton_pressed():
+func _on_SettingsButton_pressed(): #Open settings button
 	if MManimationfinished:
 		print("Settings Button Pressed")
 		MManimationPlayer.play("Settings")
@@ -39,16 +45,28 @@ func _on_SettingsButton_pressed():
 		settingspage.visible = true
 		musicslider.grab_focus()
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(anim_name): #To ensure no more than one animation is playing at once
 	MManimationfinished = true
 
-
-
-
-
-func _on_Back_pressed():
+func _on_Back_pressed(): #Settingsback to Main Menu
 	if MManimationfinished:
 		MManimationPlayer.play("SettingsBack")
 		$AnimatedSprite.speed_scale = 1
 		settingspage.visible = false
+		settingsbut.grab_focus()
+
+
+func _on_Save_pressed(): #Save settings 
+	print("Settings Button Pressed")
+
+
+func _on_BackfromPlay_pressed(): #Level menu to Main menu
+	
+	if MManimationfinished:
+		MManimationPlayer.play("Playback")
+		$AnimatedSprite.speed_scale = 1
+		playpage.visible = false
 		startbut.grab_focus()
+
+func _on_Launch_pressed():
+	get_tree().change_scene("res://Levels/Tutorials/Tutorial.tscn") 
--- Lux/Levels/Menu/SettingsControl.gd
+++ Lux/Levels/Menu/SettingsControl.gd
@@ -8,7 +8,7 @@ var master_bus = AudioServer.get_bus_index("Master")
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(master_bus, value)
 
-	if value == -10:
+	if value == -20:
 		AudioServer.set_bus_mute(master_bus, true)
 	else:
 		AudioServer.set_bus_mute(master_bus, false)
